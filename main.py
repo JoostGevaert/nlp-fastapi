efbecf7fb6b8137f9dbee45152747765f3818ddb
Joost Gevaert <jhngevaert@gmail.com>
2021-01-10 22:13:08 +0100

    half of fastapi video tutorial. next: fix typing List[str]

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from fastapi import FastAPI
+from ml import nlp
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_main():
+    return {"message": "Hello World"}
+
+class Article(BaseModel):
+    content: str
+    comments: List[str] = []
+
+@app.post("/article/")
+def analyze_article(article: Article):
+    return {"message": article.content, "comments": article.comments}
+
+
